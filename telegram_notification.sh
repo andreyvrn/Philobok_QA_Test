da5d6a5f14cb36ceeef7a79a560082dc344bb156
andreyvrn <apalikhin@mail.ru>
2021-08-03 00:42:18 +0300

    Update telegram_notification.sh

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -17,8 +17,7 @@ send_msg () {
 # Send message to the bot with some pertinent details about the job
 # Note that for Markdown, you need to escape any backtick (inline-code)
 # characters, since they're reserved in bash
-VAR1 = ${job_status}
-if [VAR1 == "success"]; then
+if [${job_status} == "success"]; then
             send_msg "
 🎉 The job was automatically triggered by a ${event_name} event.
 🐧 This job is now running on a ${runner_os} server hosted by GitHub!
