795105d1a97d9fcabd8b83f9dbd2c01f44d98649
Andrey <apalikhin@mail.ru>
2021-08-03 22:35:48 +0300

    new message

--- vk_notification.sh
+++ vk_notification.sh
@@ -15,6 +15,7 @@ send_msg () {
 # Send message to the bot with some pertinent details about the job
 # Note that for Markdown, you need to escape any backtick (inline-code)
 # characters, since they're reserved in bash
+if [[$job_status == "success"]]; then
 send_msg "
 🎉 The job was automatically triggered by a ${event_name} event.
 🐧 This job is now running on a ${runner_os} server hosted by GitHub!
@@ -22,3 +23,4 @@ send_msg "
 🖥️ Architecture: ${architecture} .
 🍏 This job's status is ${job_status}.
 "
+fi
