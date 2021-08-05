e21576347f3bece599d4bb88d7c4327fa62612d7
andreyvrn <apalikhin@mail.ru>
2021-08-05 22:28:45 +0300

    Update vk_notification.sh

--- vk_notification.sh
+++ vk_notification.sh
@@ -1,24 +1,15 @@
 #!/bin/sh
 
-# Get the token from Travis environment vars and build the bot URL:
-
 BOT_URL_VK="https://api.vk.com/method/messages.send?"
 
-
-# Define send message function. parse_mode can be changed to
-# HTML, depending on how you want to format your message:
 send_msg () {
     curl -s -X POST ${BOT_URL_VK} -d peer_id=$VK_PEER_ID \
         -d random_id=$RANDOM -d message="$1" -d access_token=$VK_TOKEN -d v=5.131
 }
 
-# Send message to the bot with some pertinent details about the job
-# Note that for Markdown, you need to escape any backtick (inline-code)
-# characters, since they're reserved in bash
-$EmojiIcon = [System.Convert]::toInt32("1F600",16)
 if [ $job_status == "success" ]; then
 send_msg "
- ${[System.Char]::ConvertFromUtf32($EmojiIcon)}
+`u{1f609}"`u{1f60a}"
 🎉 The job was automatically triggered by a ${event_name} event.
 🐧 This job is now running on a ${runner_os} server hosted by GitHub!
 💡 Python version ${repository_git} .
@@ -27,7 +18,7 @@ send_msg "
 "
 else
  send_msg "
- ${[System.Char]::ConvertFromUtf32($EmojiIcon)}
+`u{1f609}"`u{1f60a}"
 😭😭😭😭😭😭
 🎉 The job was automatically triggered by a ${event_name} event.
 🐧 This job is now running on a ${runner_os} server hosted by GitHub!
