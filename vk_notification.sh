c1e0486b66232ea6fd15e46b96412a05856749e4
andreyvrn <apalikhin@mail.ru>
2023-09-11 00:51:09 +0300

    Update vk_notification.sh

--- vk_notification.sh
+++ vk_notification.sh
@@ -8,8 +8,8 @@ BOT_URL_VK="https://api.vk.com/method/messages.send?"
 # Define send message function. parse_mode can be changed to
 # HTML, depending on how you want to format your message:
 send_msg () {
-    curl -s -X POST ${BOT_URL_VK} -d peer_id=$VK_PEER_ID \
-        -d random_id=$RANDOM -d message="$1" -d access_token=$VK_TOKEN -d v=5.131
+    curl -s -X POST ${BOT_URL_VK} -d peer_id=$PEER_ID \
+        -d random_id=0 -d message="$1" -d access_token=$VK_TOKEN -d v=5.131
 }
 
 # Send message to the bot with some pertinent details about the job
